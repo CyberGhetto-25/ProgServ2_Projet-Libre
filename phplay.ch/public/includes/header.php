d78440609c53aa682aa86b9df3c562d28c025549
CyberGhetto-25 <bridy.marc@gmail.com>
2025-11-04 10:53:34 +0100

    fix: correction on header path

--- phplay.ch/public/includes/header.php
+++ phplay.ch/public/includes/header.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/lang.php';
 ?>
 <header style="display:flex;justify-content:space-between;align-items:center;margin-bottom:1em;">
-    <h2><a href="/phplay/index.php"><?= __('app_name') ?></a></h2>
+    <h2><a href="/../index.php"><?= __('app_name') ?></a></h2>
     <nav>
         <a href="?lang=fr">🇫🇷 Français</a> |
         <a href="?lang=en">🇬🇧 English</a>
